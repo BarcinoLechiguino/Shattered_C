0dd88fa57f2fbbcebd2ba601dcc7a5d7654772da
Ángel González <angotov@gmail.com>
2022-06-15 04:11:24 +0200

    Clearing previous project.

--- Shattered_C/Source/Common.h
+++
@@ -1,8 +0,0 @@
-#ifndef __Ç_COMMON_H__
-#define __Ç_COMMON_H__
-
-#include <stdbool.h>			// C99 boolean.
-#include <stddef.h>				// NULL, size_t, etc.
-#include <stdint.h>				// int8_t, uint8_t, etc.
-
-#endif // !__Ç_COMMON_H__
